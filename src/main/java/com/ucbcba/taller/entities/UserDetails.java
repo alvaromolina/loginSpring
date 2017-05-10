dcba4bcc9485560a2be72ba2b49e2eb0a8e0f06c
Alvaro Molina <alvaromolinac@gmail.com>
2017-05-10 14:07:05 -0400

    second commit

---
+++ src/main/java/com/ucbcba/taller/entities/UserDetails.java
@@ -0,0 +1,7 @@
+package com.ucbcba.taller.entities;
+
+/**
+ * Created by amolina on 10/05/17.
+ */
+public class UserDetails {
+}
--- src/main/java/com/ucbcba/taller/repositories/UserRepository.java
+++ src/main/java/com/ucbcba/taller/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import javax.transaction.Transactional;
 
 @Transactional
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+    User findByUsername(String username);
 }
